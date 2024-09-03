da9720a0f21f26c1870d67fd7950d147544e94fa
RyannKim327 <rksesgundo123@gmail.com>
2024-09-03 15:38:35 +0800

    09-03-24 15:38

--- src/pages/chatbox.tsx
+++ src/pages/chatbox.tsx
@@ -1,92 +1,101 @@
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useState } from "react"
-import db from "../utils/firebase"
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { db, addDoc, collection, getDocs } from "../utils/firebase";
 
 interface fbitem {
-  sender: string
-  msg: string
+  sender: string;
+  msg: string;
 }
 
 const __chat = (props: any) => {
-  const you = props.sender === "you" ? "items-end" : "items-start"
+  const you = props.sender === "you" ? "items-end" : "items-start";
   return (
-    <div className={`${props.className} flex flex-col w-full ${you}`} >
-      <fieldset className={
-        `${props.sender === "you" ? "border-lime-500" : "border-black"} border-solid border-[1px] rounded-md p-2 m-2 max-w-[75%]`
-      }>
-        {
-          props.sender === "you" ? "" : (
-            <legend className="text-sm border-[1px] border-solid border-black px-2 ml-[5px] rounded-md">{props.sender}</legend>
-          )
-        }
+    <div className={`${props.className} flex flex-col w-full ${you}`}>
+      <fieldset
+        className={`${props.sender === "you" ? "border-lime-500" : "border-black"} border-solid border-[1px] rounded-md p-2 m-2 max-w-[75%]`}
+      >
+        {props.sender === "you" ? (
+          ""
+        ) : (
+          <legend className="text-sm border-[1px] border-solid border-black px-2 ml-[5px] rounded-md">
+            {props.sender}
+          </legend>
+        )}
 
         <p>
-          {
-            (props.sender === "you" ? "" : <span>&emsp;</span>)
-          }{
-            props.children
-          }
+          {props.sender === "you" ? "" : <span>&emsp;</span>}
+          {props.children}
         </p>
       </fieldset>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 const ChatBox = (props: any) => {
-  const [data, setData] = useState<fbitem[]>([])
+  const [data, setData] = useState<fbitem[]>([]);
 
   useEffect(() => {
     const fetchdata = async () => {
       try {
-        const snap = await db.collection("chats").get()
-        const items = snap.docs.map(doc => doc.data() as fbitem)
+        const snap = await getDocs(collection(db, "chats"));
+        const items: fbitem[] = snap.docs.map((doc) => doc.data()) as fbitem[];
 
-        setData(items)
-
-      } catch (e) { }
-    }
-    fetchdata()
-
-  }, [])
+        setData(items);
+      } catch (e) {
+        console.error(e)
+      }
+    };
+    fetchdata();
+  }, []);
 
   const scroll = () => {
-    const _chat = document.getElementById("chats")
+    const _chat = document.getElementById("chats");
     if (_chat) {
       // NOTE: Ginamitan ko to ng chatgpt
       _chat.scrollTo({
         top: _chat.scrollHeight,
-        behavior: "smooth"
-      })
+        behavior: "smooth",
+      });
     }
-  }
+  };
 
   const sendmsg = () => {
-    const _send = document.getElementById("sendIcon")
-    const _load = document.getElementById("loader")
-    const msg = document.getElementById("msg") as HTMLInputElement
+    const _send = document.getElementById("sendIcon");
+    const _load = document.getElementById("loader");
+    const msg = document.getElementById("msg") as HTMLInputElement;
 
     if (_send && _load && msg) {
       if (msg.value.trim() === "") {
-        return alert("No input message")
+        return alert("No input message");
       }
 
-      _send.style.display = "none"
-      _load.style.display = "inline"
-      msg.value = ""
+      _send.style.display = "none";
+      _load.style.display = "inline";
+      msg.value = "";
 
-      scroll()
+      const __send = async () => {
+        const senddata: fbitem = {
+          sender: "Kim",
+          msg: msg.value,
+        };
+        await addDoc(collection(db, "chats"), senddata);
+      };
+
+      __send();
+
+      scroll();
 
       setTimeout(() => {
-        _send.style.display = "block"
-        _load.style.display = "none"
-      }, 1500)
+        _send.style.display = "block";
+        _load.style.display = "none";
+      }, 1500);
     }
-  }
+  };
 
   setTimeout(() => {
-    scroll()
-  }, 100)
+    scroll();
+  }, 100);
   /*
     const chats = [
       {
@@ -116,28 +125,40 @@ const ChatBox = (props: any) => {
     ]
   */
   return (
-    <div className={`${props.className} flex flex-col h-full w-full box-border items-center`}>
+    <div
+      className={`${props.className} flex flex-col h-full w-full box-border items-center`}
+    >
       <h3>Chatbox</h3>
-      <div id="chats" className="flex flex-col h-full w-full box-border overflow-scroll">
-        {
-          data.map((e) => {
-            return (
-              <__chat sender={e['sender']}>{e['msg']}</__chat>
-            )
-          })
-        }
+      <div
+        id="chats"
+        className="flex flex-col h-full w-full box-border overflow-scroll"
+      >
+        {data.map((e) => {
+          return <__chat sender={e["sender"]}>{e["msg"]}</__chat>;
+        })}
       </div>
       <div className="flex w-full box-border p-2">
-        <input id="msg" className="w-full outline-none box-border bg-slate-300/50" placeholder="Enter your message" />
+        <input
+          id="msg"
+          className="w-full outline-none box-border bg-slate-300/50"
+          placeholder="Enter your message"
+        />
         <button className="px-2">
-          <FontAwesomeIcon onClick={() => { sendmsg() }} id="sendIcon" className="text-lg" icon={faPaperPlane} />
+          <FontAwesomeIcon
+            onClick={() => {
+              sendmsg();
+            }}
+            id="sendIcon"
+            className="text-lg"
+            icon={faPaperPlane}
+          />
           <span id="loader" className="hidden">
-            <div className="loader" ></div>
+            <div className="loader"></div>
           </span>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -1,17 +1,30 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
 import firebase from "firebase/compat/app";
+import { addDoc, getFirestore, collection, getDocs } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process?.env.APIKEY,
-  authDomain: process?.env.AUTH_DOMAIN,
-  projectId: process?.env.PROJECT_ID,
-  storageBucket: process?.env.STORAGE_BUCKET,
-  messagingSenderId: process?.env.MSG_SENDER_ID,
-  measurementID: process?.env.MEASUREMENT_ID,
+interface fbase {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: number;
+  appId: string;
+  measurementID: string;
+}
+
+const projID = "sulatbaybayin-website-ts";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: `AIzaSyAQz21mkkIHZwgsnGhqJhJxGGN_6nBTGKY`,
+  authDomain: `${projID}.firebaseapp.com`,
+  projectId: projID,
+  storageBucket: `${projID}.appspot.com`,
+  messagingSenderId: "685011670583",
+  appId: "1:685011670583:web:192fd1451d9041939511b2",
+  measurementId: "G-LEXEWNQ8C8",
 };
 
-initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-
-export default db;
+export { db, addDoc, collection, getDocs };
